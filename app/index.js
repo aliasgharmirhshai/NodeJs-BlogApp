26f9fee6ed5ebc1c0f6a30a68bdcec7317bc2ce4
aliasgharmirshahi <lmyrshahy3@gmail.com>
2023-05-29 20:28:35 +0330

    Start Project

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+
+module.exports = () => {
+    const port = process.env.APP_PORT;
+    app.listen(port, () => {
+        console.log(`app is running on http://localhost:${port}`)
+    });
+};
